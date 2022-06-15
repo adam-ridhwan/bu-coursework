7b7bb5c2cfb29d229b8036520b4c589bccbd20ed
Adam Ridhwan <adamridhwan.1@gmail.com>
2022-06-15 11:11:58 -0400

    update

--- BU622/AdamRidhwan_AmirHamzah_hw4/src/ThreadColor.java
+++
@@ -1,5 +0,0 @@
-public class ThreadColor {
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_RED="\u001B[31m";
-    public static final String ANSI_BLUE="\u001B[34m";
-}
